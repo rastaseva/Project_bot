19c1fa435527435142ab52ce4d6056945deb326b
Vsevolod Petrishchev <Vsevolod_Petrishchev@epam.com>
2022-05-05 17:49:02 +0300

    add bot.js, const.js

---
+++ bot.js
@@ -0,0 +1,28 @@
+const { Telegraf } = require('telegraf');
+const covidApi = require('covid19-api');
+const COUNTRIES_LIST = require('./const');
+const bot = new Telegraf('5120241314:AAGxfRGn96SGHvai6ulTp66BYkHa41FNnbc');
+
+bot.start(ctx => ctx.reply(`
+   Привет ${ctx.from.first_name}!
+   Узнай статистику по Коронавирусу.
+   Введи страну на английском языке и получи статистику.
+   Получить весь список стран можно по команде /help."
+`));
+bot.help(ctx => ctx.reply(COUNTRIES_LIST)) // список всех стран на английском языке можно взять в документации covid19-api
+bot.on('text', async (ctx) => {
+    try {
+        const userText = ctx.message.text;
+        const covidData = await covidApi.getReportsByCountries(userText);
+        const countryData = covidData[0][0];
+        const formatData = `
+        Страна: ${countryData.country},
+        Случаи: ${countryData.cases},
+        Смерти: ${countryData.deaths},
+        Выздоровело: ${countryData.recovered}`;
+        ctx.reply(formatData);
+    } catch (e) {
+        ctx.reply('Такой страны не существует, для получения списка стран используй команду /help');
+    }
+})
+bot.launch();
